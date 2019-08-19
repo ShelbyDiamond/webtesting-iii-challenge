4b0076f2956810ab96a495c6d106c1e4c894b9a7
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-19 15:35:01 -0700

    Updated to have 3 snapshot tests and
    1 render test for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from "react"
+import renderer from "react-test-renderer"
+import Dashboard from "./Dashboard"
+import * as rtl from "react-testing-library"
+
+describe("<Dashboard/>", () => {
+  it("defaults to `unlocked` and `open`", () => {
+    const tree = renderer.create(<Dashboard />)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders correctly", () => {
+    const test2 = rtl.render(<Dashboard />)
+    expect(test2.baseElement).toMatchSnapshot
+  })
+})
