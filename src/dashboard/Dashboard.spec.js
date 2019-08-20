f0426579fdd2dc26be7d3776cd19ddc4e8077d04
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-20 09:28:10 -0700

    met MVP, forgot to commit last night

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,15 +3,37 @@ import React from "react"
 import renderer from "react-test-renderer"
 import Dashboard from "./Dashboard"
 import * as rtl from "react-testing-library"
+import Controls from "./Controls"
+import { render } from "@testing-library/react"
 
-describe("<Dashboard/>", () => {
-  it("defaults to `unlocked` and `open`", () => {
-    const tree = renderer.create(<Dashboard />)
-    expect(tree.toJSON()).toMatchSnapshot()
+// describe("<Dashboard/>", () => {
+//   it("defaults to `unlocked` and `open`", () => {
+//     const tree = renderer.create(<Dashboard />)
+//     expect(tree.toJSON()).toMatchSnapshot()
+//   })
+
+//   it("renders correctly", () => {
+//     const test = rtl.render(<Controls />)
+//     expect(test.baseElement).toMatchSnapshot()
+//   })
+// })
+
+describe("Button Defaluts to unlocked and open", () => {
+  it("Defaluts to unlocked", () => {
+    const { getByText } = render(<Controls />)
+    expect(getByText(/close gate/i)).toBeTruthy()
   })
 
-  it("renders correctly", () => {
-    const test2 = rtl.render(<Dashboard />)
-    expect(test2.baseElement).toMatchSnapshot
+  it("Defaluts to unlocked", () => {
+    const { getByText } = render(<Controls />)
+    expect(getByText(/lock gate/i)).toBeTruthy()
+  })
+})
+
+describe("<Controls />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Controls />)
+
+    expect(tree.toJSON()).toMatchSnapshot()
   })
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,10 +2,22 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import Display from "./Display"
+import { render } from "@testing-library/react"
 
 describe("<Display/>", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Display />)
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
+
+describe("displaying the initial state", () => {
+  it("defaults to Unlocked", () => {
+    const { getByText } = render(<Display />)
+    expect(getByText(/unlocked/i)).toBeTruthy()
+  })
+  it("defaults to open", () => {
+    const { getByText } = render(<Display />)
+    expect(getByText(/open/i)).toBeTruthy()
   })
 })
